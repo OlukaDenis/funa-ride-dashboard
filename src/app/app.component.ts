214c45e2ab20569b346911c4420109dfdb29ef0b
Denis Oluka <olukadeno@gmail.com>
2024-10-28 23:08:51 +0300

    Add funa ride logos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,7 +12,7 @@ import { NgxSonnerToaster } from 'ngx-sonner';
   imports: [NgClass, RouterOutlet, ResponsiveHelperComponent, NgxSonnerToaster],
 })
 export class AppComponent {
-  title = 'Angular Tailwind';
+  title = 'FunaRide';
 
-  constructor(public themeService: ThemeService) {}
+  constructor(public themeService: ThemeService) { }
 }
